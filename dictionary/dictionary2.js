b5bea19f5c9bfb310ff28a96ff83ae0b5e3eeb00
Saso Solomon <solobe4u@gmail.com>
2026-01-19 20:35:07 +0100

    update dictionary and added another method

--- dictionary/dictionary2.js
+++ dictionary/dictionary2.js
@@ -2,10 +2,16 @@ let button = document.querySelector("button");
 // button.addEventListener("click", searchFun);
 let searchField = document.querySelector("input");
 let dictioary = document.querySelector(".dictionary");
+let wordy = document.querySelector(".wordy");
+let phonet = document.querySelector(".phonet");
+let audioe = document.querySelector(".audioe");
+let defin = document.querySelector(".defin");
+let exam = document.querySelector(".exam");
+let part = document.querySelector(".part");
 
     button.addEventListener("click", getData);
 
-let search = 'cook';
+let search = 'hello';
 const searchFun = async function (word1) {
      let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word1}` ;
 
@@ -14,7 +20,6 @@ const searchFun = async function (word1) {
     //  .then(res => res.json()); anoda way of writing the previous line.
 
     // console.log(resJason); it will show error at 26 if u use this
-     return resJason[0];
 
 let word = resJason[0].word;
 let phonetics = resJason[0].phonetic;
@@ -24,6 +29,8 @@ let example = resJason[0].meanings[0].definitions[0].example;
 let partOfSpeech = resJason[0].meanings[0].partOfSpeech;
 
 updateDisplay(word, phonetics, audio, meanings, example, partOfSpeech); 
+
+     return resJason[0];//this has to be at the end of the function or it will return before fetching the data(below it wont work)
 
 };
 
@@ -43,7 +50,7 @@ function getData(e) {
     let word = searchField.value;
     searchFun(word, e.currentTarget.value);
 }
-searchFun(word);
+searchFun(search);
 
 
 
