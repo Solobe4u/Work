45adc1775d98f8a3ac5dfdba330ee78b7c944e69
Saso Solomon <solobe4u@gmail.com>
2026-03-05 23:09:45 +0100

    Footer section of  coffee site and few things left to be done

--- coffeeWebsite/script.js
+++ coffeeWebsite/script.js
@@ -6,3 +6,45 @@ menuOpenBtn.addEventListener("click", () => {
 })
 
 menuCloseBtn.addEventListener("click", () => menuOpenBtn.click());// Remove the "menu-open" class from the body element when the menu close button is clicked
+
+
+
+
+// For the testimonials carousel, we are using Swiper.js library. The following code initializes the Swiper instance with the specified parameters for vertical direction, looping, pagination, navigation arrows, and scrollbar. Copied from Swiper.js documentation: https://swiperjs.com/swiper-api#parameters
+const swiper = new Swiper('.slider-wrapper', {
+    
+    /*
+   Optional parameters
+  direction: 'vertical',
+  */
+  loop: true,
+  grabCursor: true, // Change cursor to a hand when hovering over the carousel to indicate that it can be dragged
+  spaceBetween: 30, // Space between slides in px
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, // Make pagination bullets clickable
+    dynamicBullets: true, // Enable dynamic bullets for pagination where the active bullet is larger than the others
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  /*
+   And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },*/
+
+   // Responsive breakpoints
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 }
+  }
+
+});
